0f19bc84bfecdc3227da7d522b87c9f4a5d0fe07
koutsosg <g.koutsos123@gmail.com>
2022-05-21 02:04:54 +0200

    remove public file

--- public/component---src-pages-ignored-page-js-b2e754808feae92f69a3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{Heh6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),l=n.n(t),o=n("Bl7J"),r=n("vrFN"),c=n("WlC/");a.default=function(e){var a=Object(c.useI18next)();return console.log(a),l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Ignored page"}),l.a.createElement("h1",null,"Ignored page"),l.a.createElement("p",null,"This page does not have language prefix"),l.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-ignored-page-js-b2e754808feae92f69a3.js.map
